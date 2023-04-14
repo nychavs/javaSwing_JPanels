e3f6208ad5fd7d7b207ff6b6982dfe1240aa9790
nychavs <chavesnaiury@gmail.com>
2023-04-14 16:48:33 -0300

    first commit

---
+++ src/alternarjpanel/AlternarJPanel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alternarjpanel;
+
+/**
+ *
+ * @author ht3015955
+ */
+public class AlternarJPanel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    JFrame01 principal = new JFrame01();
+    principal.setVisible(true);
+ }}
+ 
